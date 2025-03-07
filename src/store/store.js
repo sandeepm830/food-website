99e55f82eabe72699e42bf11f8358cf5a08b5153
Your Name <your-email@example.com>
2025-03-07 23:22:02 +0530

    Deploy to Github Pages

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-// store/store.js
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./cartSlice";
-
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
-});
